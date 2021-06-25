d4c7aa045045cdbb4cdf67fefe3af5fcbcb28b25
= <i.ezerbaev97@gmail.com>
2021-06-25 23:44:44 +0300

    add product detail | flask login

--- shop/__init__.py
+++ shop/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
-from config import Config
+from config import *
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
 app.config.from_object(Config)
 migrate = Migrate(app, db)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 from shop import routes, models
